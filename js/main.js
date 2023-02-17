e950b8abfa43b325fd701ed4f9816fce6569889b
moa3337 <moa-x@hotmail.com>
2023-02-17 15:51:10 +0100

    setting axios

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
-const { createApp } = Vue;
 
-const app = createApp({
-    data() {
-        return {
-        };
-    },
-});
-app.mount("#app");
+
+casual_eMail = [];
+
+for (let i = 0; i < 10; i++) {
+    axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((result) => {
+            casual_eMail.push(result.data.reponse);
+
+            console.log(casual_eMail);
+            if (casual_eMail.length >= 10) {
+            }
+        });
+};

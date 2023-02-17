90730727db91af3560ce3dd4c035f819ce019aeb
moa3337 <moa-x@hotmail.com>
2023-02-17 15:59:55 +0100

    completamento random email

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ for (let i = 0; i < 10; i++) {
     axios
         .get("https://flynn.boolean.careers/exercises/api/random/mail")
         .then((result) => {
-            casual_eMail.push(result.data.reponse);
+            casual_eMail.push(result.data.response);
 
             console.log(casual_eMail);
             if (casual_eMail.length >= 10) {
